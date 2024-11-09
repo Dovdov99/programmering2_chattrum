d553aa2e4b530ca9beacb0fa0bc2c3deeaf95d75
David Sarkis <davidsarkis99@hotmail.com>
2024-11-09 18:59:28 +0100

    tillägg av kommentarer

--- client.py
+++ client.py
@@ -1,21 +1,46 @@
+"""
+Detta program implementerar en enkel chattklient som ansluter till en chattserver.
+Klienten tillåter användaren att välja ett användarnamn och skicka meddelanden till servern.
+Meddelanden från servern tas emot och skrivs ut på skärmen.
+
+Funktioner:
+- Användaren kan välja ett användarnamn.
+- Klienten kan skicka meddelanden till servern.
+- Klienten tar emot meddelanden från servern och visar dem i realtid.
+
+Användning:
+1. Starta klientprogrammet.
+2. Välj ett användarnamn.
+3. Skicka meddelanden och ta emot svar från servern.
+
+Notera: Klienten avslutas om användaren skriver "QUIT".
+"""
+
 import socket
 import threading
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 1999
+HOST = socket.gethostbyname(socket.gethostname()) #Dessa funktioner hämtar den lokala IP-adressen på maskinen programmet körs på.
+PORT = 1999 #Portnummer
 
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Skapar en klient socket och tilldelar den till en variabel.
+client_socket.connect((HOST, PORT)) #Ansluter min klient socket till en server med hjälp av HOST och PORT.
 
 
 def choose_nickname():
+    """
+    Denna funktion är till för att låta användaren välja ett användarnamn samt skicka det användarnamnet till servern.
+    """
     nickname = input("Välj ett användarnamn: ")
     client_socket.send(nickname.encode("UTF-8"))
 
 
-def recieve_message():
+def receive_message():
+    """
+    Denna funktion är till för att kunna ta emot meddelanden som skickas från servern och printa det.
+    Skulle det vara så att det blir något fel så kommer Exception fånga upp det.
+    """
     while True:
         try:
             message = client_socket.recv(1024).decode("UTF-8")
@@ -25,6 +50,10 @@ def recieve_message():
             break
 
 def write_message():
+    """
+    Denna funktion är till för att kunna skicka meddelanden till servern.
+    Villkor för att kontrollera om användaren vill lämna chatten och ifall det stämmer så stängs socketen. Annars så skickas meddelandet till servern.
+    """
     while True:
         send_message = input("")
         if send_message == "QUIT":
@@ -38,7 +67,7 @@ def write_message():
 
 
 choose_nickname()
-recieve_message_thread = threading.Thread(target=recieve_message)
-write_message_thread = threading.Thread(target=write_message)
-recieve_message_thread.start()
-write_message_thread.start()
+receive_message_thread = threading.Thread(target=receive_message) #Skapar en tråd för receive_message funktionen.
+write_message_thread = threading.Thread(target=write_message) #Skapar en tråd för write_message funktionen.
+receive_message_thread.start() #Startar tråden.
+write_message_thread.start() #Startar tråden.
--- server.py
+++ server.py
@@ -1,27 +1,50 @@
+"""
+Detta program implementerar en enkel chattserver som tillåter flera klienter att ansluta och kommunicera med varandra.
+Servern använder TCP-sockets för att hantera anslutningar och meddelanden mellan klienterna. 
+
+Funktioner:
+- Klienter kan ansluta till servern och ange ett användarnamn.
+- Servern sänder meddelanden från en klient till alla andra anslutna klienter.
+- Klienter kan lämna chattrummet genom att skicka kommandot "QUIT".
+
+Användning:
+1. Starta servern.
+2. Klienter kan ansluta till servern via dess IP-adress och portnummer.
+3. Klienter kan skicka meddelanden som kommer att broadcastas till alla andra anslutna klienter.
+"""
+
+
 import socket
 import threading
 
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 1999
+HOST = socket.gethostbyname(socket.gethostname()) #Dessa funktioner hämtar den lokala IP-adressen på maskinen programmet körs på.
+PORT = 1999 #Portnummer
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Skapar en server socket och tilldelar den till en variabel
+server_socket.bind((HOST, PORT)) #Binder server socketen jag skapat till en IP-adress samt en port.
+server_socket.listen() #Startar lyssning av servern.
 print("Server is listening for a connection...")
 
-client_list: list = []
-nickname_list: list = []
+client_list: list = [] #Skapar en tom lista där alla klienter som ansluter hamnar.
+nickname_list: list = [] #Skapar en tom lista där alla användarnamn som klienterna väljer hamnar.
 
 
-def handle_client():
+def handle_client(): 
+    """
+    Denna funktion börjar med att acceptera alla klienter som ansluter till servern för att sedan lägga till dem i klientlistan. 
+    Sedan så kommer det användarnamn klienten väljer läggas till i nickname listan.
+    Två vilkor finns för att kontrollera vad som ska skrivas ut beroende på hur många klienter som är anslutna.
+    Sist så skapar vi en tråd för varje klient.
+    Denna funktion kommer loopa oändligt.
+    """
     while True:
         client_socket, client_address = server_socket.accept()
         client_list.append(client_socket)
 
         nickname = client_socket.recv(1024).decode("UTF-8")
         nickname_list.append(nickname)
-        print(f"Användarnamn för {client_address} heter: {nickname}")
+        print(f"Användarnamn för klient: {client_address} är: {nickname}")
 
         if len(client_list) > 1:
             broadcast_message(f"{nickname} har anslutit till chatten")
@@ -29,11 +52,17 @@ def handle_client():
             message = "Du har anslutit till chatten"
             client_socket.send(message.encode("UTF-8"))
 
-        recive_message_thread = threading.Thread(target=receive_message, args=(client_socket, nickname, client_address))
+        recive_message_thread = threading.Thread(target=receive_message, args=(client_socket, nickname))
         recive_message_thread.start()
 
 
-def receive_message(client_socket, nickname, client_address):
+def receive_message(client_socket, nickname):
+    """
+    Denna funktion är till för att ta emot meddelanden från klienterna.
+    Jag använder mig av ett try block för att ta emot alla meddelanden och ifall klienten skickar QUIT så skall klienten samt deras användarnamn tas bort från respektive lista.
+    Alla andra meddelanden som klienterna skickar kommer broadcastas till alla anslutna klienter.
+    Skulle det bli error så kommer Exception fånga upp det istället för att servern ska krasha.
+    """
     while True:
         try:
             message_from_client = client_socket.recv(1024).decode("UTF-8")
@@ -47,25 +76,23 @@ def receive_message(client_socket, nickname, client_address):
             else:
                 broadcast_message(f"{nickname}: {message_from_client}")
 
-        except Exception as e:
+        except Exception:
             break
     
 
 def broadcast_message(message_from_client):
+        """
+        Denna funktion är till för att skicka ut alla meddelanden som en klient skickar till alla anslutna klienter.
+        Även här så använder jag Exception för att fånga upp errors istället för att servern ska krasha. 
+        """
         try:
             for clients in client_list:
                 clients.send(message_from_client.encode("UTF-8"))
-        except Exception as e:
+        except Exception:
             print("Fel vid sändning av meddelande")
-            print(e)
+            
             
         
     
 
-handle_client()
-
-
-
-
-
-
+handle_client() #Anropar handle_client funktionen.
